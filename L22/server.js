2fb5d3f2f91024b8f8e69bfcc1299822bd9f38dc
sunny348 <sanpreetsingh836@gmail.com>
2025-08-13 15:00:47 +0530

    L-22

---
+++ L22/server.js
@@ -0,0 +1,27 @@
+//REQUIRE OR IMPORT ALL THE MODULES HERE ONLY
+const express = require("express");
+require("dotenv").config();
+const dbConnect = require("./config/database");
+const userRouter = require("./routes/userRoutes");
+
+//SET INSTANCES HERE ONLY
+const app = express();
+
+
+
+//VARIABLE DECLARATION HERE ONLY
+const PORT = process.env.PORT || 4888;
+
+
+// I WANT TO RUN A MIDDLEWARE
+app.use(express.json());
+
+//WE WILLL MAKE ROUTES
+app.use("/api/user",userRouter)
+
+
+dbConnect().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server running on port: ", PORT);
+  });
+});
